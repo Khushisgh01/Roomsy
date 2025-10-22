9dfac4b551c08f5744625228248f5e5f89c05841
unknown <khushi096btcseai24@igdtuw.ac.in>
2025-10-22 00:12:32 -0700

    First commit

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.get("/home",(req,res)=>{
+    return res.json({"hello" : "World"})
+});
+
+const start=async()=>{
+    app.listen(8000,()=>{
+        console.log("app is listing on Port:8000")
+    });
+}
+start();
